2cf1fee4ed1dadc9eca9703cad9c4a5858d0ff19
marcinzygan <soul@o2.pl>
2022-02-01 22:08:29 +0000

    commit

--- script.js
+++ script.js
@@ -335,6 +335,6 @@ GOOD LUCK 😀
 //   console.log(humanAges, 'Avrage Age');
 // };
 
-const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
-const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
-console.log(avg1, avg2);
+// const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+// const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+// console.log(avg1, avg2);
